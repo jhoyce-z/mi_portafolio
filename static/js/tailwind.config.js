e5d73c9a4b8e87089fe0aec73ce9b13e25ca0257
jhoyce-z <jhozky@gmail.com>
2025-11-25 10:44:59 -0500

    agregando commit

---
+++ static/js/tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#0df269",
+        "background-light": "#f5f8f7",
+        "background-dark": "#102217",
+      },
+      fontFamily: {
+        "display": ["Manrope", "sans-serif"],
+      },
+      borderRadius: {
+        "DEFAULT": "0.25rem",
+        "lg": "0.5rem",
+        "xl": "0.75rem",
+        "full": "9999px",
+      },
+    },
+  },
+};
